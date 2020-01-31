42fe76dde1c4bae3e05b8dcf212f5c9898e5fcfb
Thespyinthehole <joshuaaubrey@talktalk.net>
2020-01-31 20:40:10 +0000

    Finally got data sending and receieveing to work

---
+++ NoSpaceToRun/Assets/AirConsoleScripts/SayHello.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class SayHello : MonoBehaviour
+{
+    void Start()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        string action = (string)data["action"];
+        int amount = (int)data["info"]["amount"];
+        float torque = (float)data["info"]["torque"];
+        Debug.Log("Got data: " + action);
+    }
+}
