1b718d3ab55224699cca64a21b8714ee3e8c6e9e
Thespyinthehole <joshuaaubrey@talktalk.net>
2020-01-31 22:17:31 +0000

    Added the player joining and leaving

---
+++ NoSpaceToRun/Assets/GameController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    public Dictionary<int, PlayerActions> players = new Dictionary<int, PlayerActions>();
+
+    void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onReady += OnReady;
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
+    }
+
+    void OnReady(string code)
+    {
+        //Since people might be coming to the game from the AirConsole store once the game is live, 
+        //I have to check for already connected devices here and cannot rely only on the OnConnect event 
+        List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
+        foreach (int deviceID in connectedDevices)
+        {
+            AddNewPlayer(deviceID);
+        }
+    }
+
+    void OnConnect(int device)
+    {
+        AddNewPlayer(device);
+    }
+
+    void OnDisconnect(int device)
+    {
+        RemovePlayer(device);
+    }
+
+    private void RemovePlayer(int deviceID)
+    {
+
+        if (!players.ContainsKey(deviceID))
+        {
+            return;
+        }
+
+        //Instantiate player prefab, store device id + player script in a dictionary
+        PlayerActions pa = players[deviceID];
+        pa.Left();
+        Destroy(pa.gameObject);
+    }
+
+    private void AddNewPlayer(int deviceID)
+    {
+
+        if (players.ContainsKey(deviceID))
+        {
+            return;
+        }
+
+        //Instantiate player prefab, store device id + player script in a dictionary
+        GameObject newPlayer = Instantiate(playerPrefab, transform.position, transform.rotation) as GameObject;
+        players.Add(deviceID, newPlayer.GetComponent<PlayerActions>());
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        //When I get a message, I check if it's from any of the devices stored in my device Id dictionary
+        if (players.ContainsKey(from) && data["action"] != null)
+        {
+            //I forward the command to the relevant player script, assigned by device ID
+            players[from].ParseData(data["action"],data["info"]);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+            AirConsole.instance.onReady -= OnReady;
+            AirConsole.instance.onConnect -= OnConnect;
+        }
+    }
+}
---
+++ NoSpaceToRun/Assets/PlayerActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+public class PlayerActions : MonoBehaviour
+{
+    public void ParseData(JToken action, JToken info) 
+    {
+        Debug.Log("Action: " + action + ", Info: " + info);
+    }
+
+    public void Left()
+    {
+
+    }
+}
